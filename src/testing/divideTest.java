842a75d5137a207cf22c1264e07f4645f76054da
Tejaswini1111 <105067769+Tejaswini1111@users.noreply.github.com>
2022-05-09 09:00:16 +0530

    Added test cases
    Added test cases

---
+++ src/testing/divideTest.java
@@ -0,0 +1,16 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class divideTest {
+
+	@Test
+	void test() {
+		JunitTesting test = new JunitTesting();
+		double output = test.divide(10, 10);
+		assertEquals(1, output);
+	}
+
+}
